b8a21a001685141636344e969d4773673aed6630
Divyeshkalariya <divyeshkalariya26@gmail.com>
2023-05-05 18:41:59 +0530

    add data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FontAwesome from 'react-fontawesome';
+const myimg = require('./images/application.gif')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+reportWebVitals();
+
+const root = ReactDOM.createRoot(document.getElementById('external'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <section id='regform'>
+      <div className='main'>
+        <div className='content1'>
+          <h1>Regirstation Application </h1>
+          <hr />
+          <img src={myimg} alt="my-image"></img>
+        </div>
+        <div className='content2'>
+          <h1>Application form</h1>
+          <hr />
+          <form>
+            <input type='text' name='requiredname' placeholder='Name *' pattern='[a-zA-Z]$' required/> <br /><br />
+            <input type='email' name='requiredemail' placeholder='Email *' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$" required/> <br /><br />
+            <input type='text' name='requiredphone' placeholder='Phone No. *' pattern="[0-9]{10}$" required/> <br /><br />
+            <textarea rows='7' placeholder='Message'></textarea> <br /><br />
+            <input type='submit' name='submit' value='Submit' />
+            <input type='reset' name='reset' value='Reset' />
+          </form>
+        </div>
+      </div>
+    </section>
+    <section id="address">
+      <p> <i className='fa fa-map-marker'></i> &nbsp; <b>Address : </b> 501, Mauryansh Elanza, 5th Floor, Nr Parekh Hospital Nr, Shyamal Cross Rd, Satellite, Ahmedabad, Gujarat 380015</p>
+      <p> <i className='fa fa-envelope'></i> &nbsp; <b>Email : </b> <a href='mailto:netclus@gmail.com'>netclus@gmail.com</a></p>
+      <p> <i className='fa fa-phone-square'></i> &nbsp; <b>Contact : </b> 079 6663 7443</p>
+    </section>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
